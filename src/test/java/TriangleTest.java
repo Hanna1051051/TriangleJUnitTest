20b38946dd5663096511f62a62243cf80cb73128
Hanna1051051 <70807384+Hanna1051051@users.noreply.github.com>
2021-09-10 04:24:39 +0300

    Update TriangleTest.java

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -33,7 +33,7 @@ public class TriangleTest {
             "5, 3, 1",
             "5, 2, 7",
     })
-    void squareTest(double a, double b, double c) {
+    void squareTest(int a, int b, int c) {
         double square = Triangle.triangleSquare(a, b, c);
         double per = (a + b + c) / 2;
         Assertions.assertEquals((Math.sqrt(per * (per - a) * (per - b) * (per - c))), square);
