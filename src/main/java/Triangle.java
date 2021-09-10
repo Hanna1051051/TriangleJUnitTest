49bdd8437f4722a624910234814b9005c716e84d
Hanna1051051 <70807384+Hanna1051051@users.noreply.github.com>
2021-09-10 04:24:12 +0300

    Update Triangle.java

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,6 +1,6 @@
 public class Triangle {
 
- public static double triangleSquare(double a, double b, double c ){
+ public static double triangleSquare(int a, int b, int c ){
    double per2=(a+b+c)/2;
      return (Math.sqrt(per2*(per2-a)*(per2-b)*(per2-c)));
  }
